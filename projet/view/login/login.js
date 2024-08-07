edf8637562842d80f5d33b941ffe1353c8cfd236
Deve1loper <doudanesarah1@gmail.com>
2024-08-07 18:02:57 +0100

    Delete projet directory

--- projet/view/login/login.js
+++
@@ -1,5 +0,0 @@
-const sign_in_btn = document.querySelector("#sign-in-btn");
-const sign_up_btn = document.querySelector("#sign-up-btn");
-const container = document.querySelector(".container");
-const sign_in_btn2 = document.querySelector("#sign-in-btn2");
-const sign_up_btn2 = document.querySelector("#sign-up-btn2");
